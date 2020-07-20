57e335e1e2feffc7c50b37768603455d25d7c394
Abdelhamid <3b7ameed@gmail.com>
2020-07-20 23:19:18 +0000

    dynamic port using

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express')
 const app = express()
 
+const PORT = process.env.PORT || 5000
+
+
+
+
 
 app.get('/', (req, res) => {
   res.send({ hi: 'there' })
 })
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
   console.log('Server is running at localhost:5000')
 })
