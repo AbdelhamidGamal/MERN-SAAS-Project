eb70dfe2b455d68ac2b62aa35b23ea702a543c13
Abdelhamid <3b7ameed@gmail.com>
2020-07-22 19:49:13 +0000

    auth production ready

---
+++ config/prod.js
@@ -0,0 +1,6 @@
+module.exports = {
+  GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+  GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.mongoURI,
+  cookieKey: process.env.cookieKey
+};
